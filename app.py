e7117ad7c81fd27aa92643d15f07796e0777ad9f
kseniaglivko <k.glivko@mail.ru>
2021-08-23 18:22:57 +0300

    Добавлен приветственный скрипт, исправлен запуск программы.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Модуль для работы с приложением."""
-from typing import List
+from typing import List, Any
 from fastapi import FastAPI
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -14,7 +14,7 @@ Base = declarative_base()
 Base.metadata.create_all(bind=engine)
 
 
-def get_db():
+def get_db() -> None:
     """Управление сессиями."""
     db = SessionLocal()
     try:
@@ -23,8 +23,14 @@ def get_db():
         db.close()
 
 
+@app.get("/")
+async def root() -> dict:
+    """Приветственный скрипт."""
+    return {"message": "Welcome to ExpressDelivery!"}
+
+
 @app.post("/deliveries/", response_model=Delivery)
-async def create_delivery(delivery: Delivery):
+async def create_delivery(delivery: Delivery) -> Any:
     """Создание или изменение заказа."""
     query = deliveries.insert().values(id=delivery.id, status=delivery.status)
     last_record_id = await database.execute(query)
@@ -32,7 +38,7 @@ async def create_delivery(delivery: Delivery):
 
 
 @app.get("/deliveries/", response_model=List[Delivery])
-async def fetch_db():
+async def fetch_db() -> None:
     """Вывод информации о заказах."""
     query = deliveries.select()
     return await database.fetch_all(query)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Модуль запуска программы."""
-
+import uvicorn
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    uvicorn.run(app, host="127.0.0.1", port=5050)
