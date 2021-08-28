39445ed4804af3906ad7f824ad3df7e66f15e251
kseniaglivko <k.glivko@mail.ru>
2021-08-28 17:35:09 +0300

    Разработан locust-файл, реализовано соединение с БД и валидация входных данных.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Модуль для работы с приложением."""
-from typing import List, Any
+from typing import List, Any, Generator
 from fastapi import FastAPI
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -14,13 +14,25 @@ Base = declarative_base()
 Base.metadata.create_all(bind=engine)
 
 
-def get_db() -> None:
+def get_db() -> Generator:
     """Управление сессиями."""
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    """Соединение с БД."""
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    """Отключение от БД."""
+    await database.disconnect()
 
 
 @app.get("/")
@@ -34,11 +46,14 @@ async def create_delivery(delivery: Delivery) -> Any:
     """Создание или изменение заказа."""
     query = deliveries.insert().values(id=delivery.id, status=delivery.status)
     last_record_id = await database.execute(query)
-    return {**delivery.dict(), "id": last_record_id}
+    print({**delivery.dict(), "id": last_record_id})
 
 
 @app.get("/deliveries/", response_model=List[Delivery])
 async def fetch_db() -> None:
     """Вывод информации о заказах."""
     query = deliveries.select()
-    return await database.fetch_all(query)
+    records = await database.fetch_all(query=query)
+    for entry in records:
+        print(dict(entry.items()))
+
--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 """Модуль для работы с базой данных."""
-from typing import Set
+from enum import Enum
 from pydantic import BaseModel, constr
 from sqlalchemy import create_engine, Column, String, Table, MetaData
-from sqlalchemy_utils.types.choice import ChoiceType
 import databases
 
 DATABASE_URL = "postgresql://dbuser:dbpassword@localhost/express_delivery"
+
+DELIVERY_ID_TYPE = constr(regex="^[a-z0-9]{2,5}$")
 
 database = databases.Database(DATABASE_URL)
 
@@ -16,25 +17,23 @@ deliveries = Table(
     "deliveries",
     metadata,
     Column("id", String(5), primary_key=True, index=True),
-    Column(
-        "status",
-        ChoiceType(
-            [("to_do", "to_do"), ("in_progress", "in_progress"), ("done", "done")],
-            impl=String(),
-        ),
-        comment="Status",
-    ),
-)
+    Column("status", String(11), comment="Status"))
 
 engine = create_engine(DATABASE_URL)
 
 metadata.create_all(engine)
 
-delivery_id_type = constr(regex="^[a-z0-9]{2,5}$")
+
+class StatusEnum(str, Enum):
+    """Класс для валидации возможных статусов заказа."""
+
+    to_do = "to_do"
+    in_progress = "in_progress"
+    done = "done"
 
 
 class Delivery(BaseModel):
     """Класс для валидации тела запроса."""
 
-    id: str
-    status: Set[delivery_id_type]
+    id: DELIVERY_ID_TYPE  # type: ignore
+    status: StatusEnum
